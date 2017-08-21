5abe6be1428cb8051932026ca519714d8ee46776
dzsoniriko <dzsoniriko@gmail.com>
2017-08-21 22:56:37 +0200

    check point in case of problem in the next refactor

--- src/test/java/hu/akoel/neurnet/NeuronTest.java
+++ src/test/java/hu/akoel/neurnet/NeuronTest.java
@@ -1,6 +1,11 @@
 package hu.akoel.neurnet;
 
+import java.util.ArrayList;
+
+import hu.akoel.neurnet.neuron.ANeuron;
+import hu.akoel.neurnet.neuron.InnerNeuron;
 import hu.akoel.neurnet.neuron.InputNeuron;
+import hu.akoel.neurnet.neuron.OutputNeuron;
 import hu.akoel.neurnet.strategies.StaticDefaultWeightStrategy;
 import junit.framework.Test;
 import junit.framework.TestCase;
@@ -16,12 +21,15 @@ public class NeuronTest extends TestCase{
         return new TestSuite( NeuronTest.class );
     }
 
+    // -------------
+    // --- Input ---
+    // -------------
     public void testInputNeuronSetterGetterPairs(){
     	Double[][] dataPairs = {
-    			//{input, w, δ}
-    			{0.0, 0.5, 0.0},
-    			{0.0, 0.5, 0.3},
-    			{0.0, 0.5, 0.7},
+    			//{input, w, σ, δ}
+    			{0.0, 0.5, 0.0, 2.3},
+    			{0.0, 0.5, 0.3, 0.0},
+    			{0.0, 0.5, 0.7, -1.7},
     	};
 
     	for (int i = 0; i < dataPairs.length; i++ ){
@@ -32,30 +40,35 @@ public class NeuronTest extends TestCase{
     		assertEquals( dataPairs[i][1], inputNeuron.getWeight() );
     		
     		// --- input ---
-    		inputNeuron.setInput(dataPairs[i][1]);	
+    		inputNeuron.setInput( dataPairs[i][1] );	
     		assertEquals( dataPairs[i][1], inputNeuron.getInput() );
-    	
+
+    		// --- σ ---
+    		inputNeuron.setSigma( dataPairs[i][2] );
+    		assertEquals( dataPairs[i][2], inputNeuron.getSigma() );
+
     		// --- δ ---
-    		inputNeuron.calculateWeight(dataPairs[i][2], 0.5, 0.7 );
-    		assertEquals( dataPairs[i][2], inputNeuron.getDelta() );
+    		inputNeuron.calculateWeight(dataPairs[i][3], 0.5, 0.7 );
+    		assertEquals( dataPairs[i][3], inputNeuron.getDelta() );
+    		
     		
     	}    	
     }
     
     public void testInputNeuronCalculateOutputMethod(){
     	Double[][] dataPairs = {
-    			//{w, Input, expectedValue}
-    			{10.0, 0.0, 0.5},
-    			{10.0, 1.0, 0.9999546021312976},
-    			{-10.0, 1.0, 4.539786870243442E-5},
+    			//{Input, w, expectedValue}
+    			{0.0, 10.0, 0.5},
+    			{1.0, 10.0, 0.9999546021312976},
+    			{1.0, -10.0, 4.539786870243442E-5},
     			{2.0, 2.0, 0.9820137900379085},
-    			{2.0, -2.0, 0.017986209962091562},
+    			{-2.0, 2.0, 0.017986209962091562},
     	
     	};
     	for (int i = 0; i < dataPairs.length; i++ ){
     		InputNeuron inputNeuron = new InputNeuron();
-    		inputNeuron.setWeight( new StaticDefaultWeightStrategy( dataPairs[i][0] ) );
-    		inputNeuron.setInput( dataPairs[i][1] );
+    		inputNeuron.setWeight( new StaticDefaultWeightStrategy( dataPairs[i][1] ) );
+    		inputNeuron.setInput( dataPairs[i][0] );
     		inputNeuron.calculateOutput();
     		    		
     		assertEquals( dataPairs[i][2], inputNeuron.getSigma() );   		
@@ -85,4 +98,193 @@ public class NeuronTest extends TestCase{
     	}
     	
     }
+    
+    // --------------
+    // --- Inner ---
+    // --------------
+    public void testInnerNeuronSetterGetterPairs(){
+    	Double[][] dataPairs = {
+    			//{w, σ, δ}
+    			{0.5, 0.0, 0.3},
+    			{0.0, 0.3, 0.5},
+    			{0.0, 0.0, 0.7},
+    			{0.3, 0.5, 1.1},
+    	};
+    	double α = 0.5;
+    	double β = 0.7;    	
+    	double input = 2.3;
+    	double w = -7.5;
+    	InputNeuron inputNeuron = new InputNeuron();    	
+    	ArrayList<ANeuron> inputNeuronList = new ArrayList<ANeuron>();
+    	inputNeuronList.add(inputNeuron);
+    	inputNeuron.setInput( input );
+    	inputNeuron.setWeight( new StaticDefaultWeightStrategy( w ) );   	
+    	
+    	for (int i = 0; i < dataPairs.length; i++ ){    		
+    		InnerNeuron innerNeuron = new InnerNeuron();    		
+    		
+    		// --- w ---
+    		innerNeuron.connectToPreviousNeuron(inputNeuronList.iterator(), new StaticDefaultWeightStrategy( dataPairs[i][2] ) );
+    		assertEquals( dataPairs[i][2], innerNeuron.getNeuronValues(0).getW_t() );
+    		
+       		// --- σ ---
+    		innerNeuron.setSigma( dataPairs[i][1] );
+    		assertEquals( dataPairs[i][1], innerNeuron.getSigma() );
+    		
+    		// --- δ ---
+    		innerNeuron.calculateWeight(dataPairs[i][2], α, β );
+    		assertEquals( dataPairs[i][2], innerNeuron.getDelta() );
+    		
+    	}    	
+    }
+    
+    public void testInnerNeuronCalculateOutputMethod(){
+    	Double[][] dataPairs = {
+    			//{input (σ), w, expected}
+    			{0.0, 10.0, 0.5},
+    			{1.0, 10.0, 0.9999546021312976},
+    			{1.0, -10.0, 4.539786870243442E-5},
+    			{2.0, 2.0, 0.9820137900379085},
+    			{-2.0, 2.0, 0.017986209962091562},
+    	};
+    	double w = 10;
+    	InputNeuron inputNeuron = new InputNeuron();    	
+    	ArrayList<ANeuron> inputNeuronList = new ArrayList<ANeuron>();
+    	inputNeuronList.add(inputNeuron);
+    	inputNeuron.setWeight( new StaticDefaultWeightStrategy( w ) );   	
+
+    	for (int i = 0; i < dataPairs.length; i++ ){    		
+    		inputNeuron.setSigma( dataPairs[i][0] );
+    		
+    		InnerNeuron innerNeuron = new InnerNeuron();    		
+    		innerNeuron.connectToPreviousNeuron(inputNeuronList.iterator(), new StaticDefaultWeightStrategy( dataPairs[i][1] ) );
+    		innerNeuron.calculateOutput();
+    		    		
+    		assertEquals( dataPairs[i][2], innerNeuron.getSigma() );    		
+    	}    	
+    }
+    
+    public void testInnerNeuronCalculateWeightMethod(){
+    	Double[][] dataPairs = {
+    			//{input, w, δ, α, β, expectedValue}
+    												//β=0.0
+    			{1.0, 0.2, 0.5, 1.0, 0.0, 0.7},
+    			{1.0, 0.2, 0.5, 0.5, 0.0, 0.45},
+    			{1.0, 0.2, 0.5, 0.0, 0.0, 0.2},		//α=0.0
+    			{1.0, 0.2, 0.0, 0.5, 0.0, 0.2},		//δ=0.0
+    			{0.5, 0.0, 0.3, 0.7, 0.0, 0.105},	//w=0.0    	
+    	};
+    	double w = 10;
+    	InputNeuron inputNeuron = new InputNeuron();    	
+    	ArrayList<ANeuron> inputNeuronList = new ArrayList<ANeuron>();
+    	inputNeuronList.add(inputNeuron);
+    	inputNeuron.setWeight( new StaticDefaultWeightStrategy( w ) );   	
+    	
+    	for (int i = 0; i < dataPairs.length; i++ ){
+    		
+    		inputNeuron.setSigma( dataPairs[i][0] );
+    		
+    		InnerNeuron innerNeuron = new InnerNeuron();    		
+    		innerNeuron.connectToPreviousNeuron(inputNeuronList.iterator(), new StaticDefaultWeightStrategy( dataPairs[i][1] ) );
+    		innerNeuron.calculateWeight(dataPairs[i][2], dataPairs[i][3], dataPairs[i][4]);
+    		
+    		assertEquals( dataPairs[i][5], innerNeuron.getNeuronValues(0).getW_t() );   		
+    	}    	
+    }
+    
+    // --------------
+    // --- Output ---
+    // --------------
+    public void testOutputNeuronSetterGetterPairs(){
+    	Double[][] dataPairs = {
+    			//{w, σ, δ}
+    			{0.5, 0.0, 0.3},
+    			{0.0, 0.3, 0.5},
+    			{0.0, 0.0, 0.7},
+    			{0.3, 0.5, 1.1},
+    	};
+    	double α = 0.5;
+    	double β = 0.7;    	
+    	double input = 2.3;
+    	double w = -7.5;
+    	InputNeuron inputNeuron = new InputNeuron();    	
+    	ArrayList<ANeuron> inputNeuronList = new ArrayList<ANeuron>();
+    	inputNeuronList.add(inputNeuron);
+    	inputNeuron.setInput( input );
+    	inputNeuron.setWeight( new StaticDefaultWeightStrategy( w ) );   	
+    	
+    	for (int i = 0; i < dataPairs.length; i++ ){    		
+    		OutputNeuron outputNeuron = new OutputNeuron();    		
+    		
+    		// --- w ---
+    		outputNeuron.connectToPreviousNeuron(inputNeuronList.iterator(), new StaticDefaultWeightStrategy( dataPairs[i][2] ) );
+    		assertEquals( dataPairs[i][2], outputNeuron.getNeuronValues(0).getW_t() );
+    		
+       		// --- σ ---
+    		outputNeuron.setSigma( dataPairs[i][1] );
+    		assertEquals( dataPairs[i][1], outputNeuron.getSigma() );
+    		
+    		// --- δ ---
+    		outputNeuron.calculateWeight(dataPairs[i][2], α, β );
+    		assertEquals( dataPairs[i][2], outputNeuron.getDelta() );
+    		
+    	}    	
+    }
+    
+    public void testOutputNeuronCalculateOutputMethod(){
+    	Double[][] dataPairs = {
+    			//{input (σ), w, expected}
+    			{0.0, 10.0, 0.5},
+    			{1.0, 10.0, 0.9999546021312976},
+    			{1.0, -10.0, 4.539786870243442E-5},
+    			{2.0, 2.0, 0.9820137900379085},
+    			{-2.0, 2.0, 0.017986209962091562},
+    	};
+    	double w = 10;
+    	InputNeuron inputNeuron = new InputNeuron();    	
+    	ArrayList<ANeuron> inputNeuronList = new ArrayList<ANeuron>();
+    	inputNeuronList.add(inputNeuron);
+    	inputNeuron.setWeight( new StaticDefaultWeightStrategy( w ) );   	
+
+    	for (int i = 0; i < dataPairs.length; i++ ){    		
+    		inputNeuron.setSigma( dataPairs[i][0] );
+    		
+    		OutputNeuron outputNeuron = new OutputNeuron();    		
+    		outputNeuron.connectToPreviousNeuron(inputNeuronList.iterator(), new StaticDefaultWeightStrategy( dataPairs[i][1] ) );
+    		outputNeuron.calculateOutput();
+    		    		
+    		assertEquals( dataPairs[i][2], outputNeuron.getSigma() );    		
+    	}    	
+    }
+    
+    public void testOutputNeuronCalculateWeightMethod(){
+    	Double[][] dataPairs = {
+    			//{input, w, δ, α, β, expectedValue}
+    												//β=0.0
+    			{1.0, 0.2, 0.5, 1.0, 0.0, 0.7},
+    			{1.0, 0.2, 0.5, 0.5, 0.0, 0.45},
+    			{1.0, 0.2, 0.5, 0.0, 0.0, 0.2},		//α=0.0
+    			{1.0, 0.2, 0.0, 0.5, 0.0, 0.2},		//δ=0.0
+    			{0.5, 0.0, 0.3, 0.7, 0.0, 0.105},	//w=0.0    	
+    	};
+    	double w = 10;
+    	InputNeuron inputNeuron = new InputNeuron();    	
+    	ArrayList<ANeuron> inputNeuronList = new ArrayList<ANeuron>();
+    	inputNeuronList.add(inputNeuron);
+    	inputNeuron.setWeight( new StaticDefaultWeightStrategy( w ) );   	
+    	
+    	for (int i = 0; i < dataPairs.length; i++ ){
+    		
+    		inputNeuron.setSigma( dataPairs[i][0] );
+    		
+    		OutputNeuron outputNeuron = new OutputNeuron();    		
+    		outputNeuron.connectToPreviousNeuron(inputNeuronList.iterator(), new StaticDefaultWeightStrategy( dataPairs[i][1] ) );
+    		outputNeuron.calculateWeight(dataPairs[i][2], dataPairs[i][3], dataPairs[i][4]);
+    		
+    		assertEquals( dataPairs[i][5], outputNeuron.getNeuronValues(0).getW_t() );   		
+    	}   
+    }
+    
+    
+
 }
